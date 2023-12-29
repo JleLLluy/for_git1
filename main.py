8aa6cd8ea9832f75f7559d8e78bd43602358b976
JleLLluy <151763535+JleLLluy@users.noreply.github.com>
2023-12-29 20:43:41 +0300

    za 29.12

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import math
+import random
 
 pygame.init()
 difficulty = []
@@ -10,7 +11,7 @@ difficulty = []
 class Player(pygame.sprite.Sprite):
     sprite = pygame.image.load("player_pyg.png")
 
-    def __init__(self, *group, posx, posy, max_x, max_y):
+    def __init__(self, *group, max_x, max_y):
         super().__init__(*group)
 
         self.total_speed = 3
@@ -19,8 +20,7 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(Player.sprite, self.size)
         self.speed = [0, 0]
         self.rect = self.image.get_rect()
-        self.rect.x = posx
-        self.rect.y = posy
+        self.rect.center = [max_x // 2, max_y // 2]
         self.max_x = max_x
         self.max_y = max_y
 
@@ -38,7 +38,7 @@ class Player(pygame.sprite.Sprite):
                           pygame.key.get_pressed()[pygame.K_w] * -self.total_speed]
 
         if args and args[0].type == pygame.MOUSEBUTTONDOWN:
-            Bullet(bullets, pos_player=[self.rect.x, self.rect.y], pos_click=args[0].pos)
+            Bullet(bullets, pos_player=self.rect.center, pos_click=args[0].pos)
         if not (0 <= self.rect.x + self.speed[0] <= self.max_x - self.size[0]):
             self.speed[0] = 0
         if not (0 <= self.rect.y + self.speed[1] <= self.max_y - self.size[0]):
@@ -47,7 +47,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.rect.move(self.speed)
 
     def position(self):
-        return self.rect
+        return list(self.rect.center)
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -56,11 +56,12 @@ class Bullet(pygame.sprite.Sprite):
     def __init__(self, *group, pos_player, pos_click):
         super().__init__(*group)
 
-        self.total_speed = 4
-        self.image = pygame.transform.scale(Bullet.sprite, (32, 32))
+        self.total_speed = 7
+        self.size = (32, 32)
+        self.image = pygame.transform.scale(Bullet.sprite, self.size)
 
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = pos_player
+        self.rect.center = pos_player
 
         self.dir = (pos_click[0] - pos_player[0], pos_click[1] - pos_player[1])
         self.pos = pos_player
@@ -74,38 +75,38 @@ class Bullet(pygame.sprite.Sprite):
 
     def update(self, *args):
 
-        self.rect.x = self.pos[0] + self.speed[0]
-        self.rect.y = self.pos[1] + self.speed[1]
+        self.rect.center = [self.pos[0] + self.speed[0], self.pos[1] + self.speed[1]]
         self.pos = [self.pos[0] + self.speed[0], self.pos[1] + self.speed[1]]
 
 
-class Enemies(pygame.sprite.Sprite):
-    sprite = pygame.image.load("KNGHT_ONE.png")
+class Enemy_one(pygame.sprite.Sprite):
+    sprite = pygame.image.load("enemy_one_pygame_proj.png")
 
     def __init__(self, *group, pos_spawn):
         super().__init__(*group)
 
-        self.image = pygame.transform.scale(Enemies.sprite, (32, 32))
+        self.total_speed = 1
+        self.size = (32, 32)
+        self.image = pygame.transform.scale(Enemy_one.sprite, self.size)
 
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = pos_spawn
+        self.rect.center = pos_spawn
         self.pos = pos_spawn
-        self.speed = 3
 
-    def update(self, *args, pos_player):
+        self.dir = [0, 0]
+        self.speed = [0, 0]
 
-        if self.pos[0] >= pos_player[0]:
-            self.pos[0] += self.speed * abs(self.pos[0] - pos_player[0])\
-                           / ((self.pos[0] - pos_player[0]) ** 2 + (self.pos[1] - pos_player[1]) ** 2) ** 0.5
+    def update(self, pos_player):
+        self.dir = (pos_player[0] - self.pos[0], pos_player[1] - self.pos[1])
+        len = math.hypot(*self.dir)
+        if len == 0:
+            self.dir = (0, -1)
         else:
-            self.pos[0] += self.speed * abs(self.pos[0] - pos_player[0]) \
-                           / ((self.pos[0] - pos_player[0]) ** 2 + (self.pos[1] - pos_player[1]) ** 2) ** 0.5
-        if self.pos[1] >= pos_player[1]:
-            self.pos[1] += self.speed * abs(self.pos[1] - pos_player[1])\
-                           / ((self.pos[0] - pos_player[0]) ** 2 + (self.pos[1] - pos_player[1]) ** 2) ** 0.5
-        else:
-            self.pos[1] += self.speed * abs(self.pos[1] - pos_player[1]) \
-                           / ((self.pos[0] - pos_player[0]) ** 2 + (self.pos[1] - pos_player[1]) ** 2) ** 0.5
+            self.dir = (self.dir[0] / len, self.dir[1] / len)
+        self.speed = [self.total_speed * self.dir[0], self.total_speed * self.dir[1]]
+
+        self.rect.center = [self.pos[0] + self.speed[0], self.pos[1] + self.speed[1]]
+        self.pos = [self.pos[0] + self.speed[0], self.pos[1] + self.speed[1]]
 
 
 fps = 180
@@ -119,21 +120,22 @@ screen_main = pygame.display.set_mode(size)
 player = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
-Player(player, posx=infoObject.current_w // 2, posy=infoObject.current_h // 2,
-       max_x=infoObject.current_w, max_y=infoObject.current_h)
+Player(player, max_x=infoObject.current_w, max_y=infoObject.current_h)
+Enemy_one(enemies, pos_spawn=[random.randint(0, width), random.randint(0, height)])
+
 
 
 while running:
+    Enemy_one(enemies, pos_spawn=[random.randint(0, width), random.randint(0, height)]) #включать на свой страх и риск
     clock.tick(fps)
     player.update()
     bullets.update()
-    enemies.update(player.sprites()[0].position())
+    enemies.update(pos_player=player.sprites()[0].position())
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             player.update(event)
-            enemies.update(event)
         if event.type == pygame.KEYUP:
             player.update(event)
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -141,11 +143,13 @@ while running:
     for bullet in bullets:
         if not screen_main.get_rect().collidepoint([bullet.rect.x, bullet.rect.y]):
             bullets.remove(bullet)
-    for enemy in enemies:
-        if not screen_main.get_rect().collidepoint([enemy.rect.x, enemy.rect.y]):
-            pass
+        for enemy in enemies:
+            if (bullet.rect).colliderect(enemy.rect):
+                enemy.kill()
+                bullet.kill()
 
     screen_main.fill(pygame.Color("black"))
     player.draw(screen_main)
     bullets.draw(screen_main)
+    enemies.draw(screen_main)
     pygame.display.flip()
